cb30f55c14f40b53bcbd400eafdf92d8eca1be7d
Zaqueu <zaqueunilton1@gmail.com>
2024-06-11 20:11:46 -0300

    advances and struggles with dayjs

--- yourkspace/scripts/script.js
+++ yourkspace/scripts/script.js
@@ -1,6 +1,16 @@
 import { enableDarkMode, darkModeEnabled } from "./darkMode.js";
 import { preventTextSelected } from "./preventTitle.js";
 preventTextSelected();
-
-const title = document.querySelector('.title');
+const title = document.querySelector('.title-content');
 title.addEventListener('click', enableDarkMode);
+ 
+const dayjs = require('dayjs');
+const actualDate = dayjs();
+
+const firstDay = dayjs(actualDate).startOf('year');
+const pastDays = dataAtual.diff(firstDay, 'day');
+
+const totalDays = dataAtual.isLeapYear() ? 366 : 365;
+const percentagePassed = (pastDays / totalDays) * 100;
+
+console.log(`Progresso do ano: ${percentagePassed.toFixed(2)}%`);
